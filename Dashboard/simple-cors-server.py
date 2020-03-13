51cc958d5414d96482877e65e9711ca89f33ffd1
Yun-RMDS <yunchunnan@gmail.com>
2020-03-13 11:14:39 -0700

    Corona Dashboard

---
+++ Dashboard/simple-cors-server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""Use instead of `python3 -m http.server` when you need CORS"""
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', 'http://127.0.0.1:8081')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        return super(CORSRequestHandler, self).end_headers()
+
+
+httpd = HTTPServer(('127.0.0.1', 8001), CORSRequestHandler)
+httpd.serve_forever()
