1fa13c37d57e91c0147f08394a0616d136c7a582
teoten <teoten@gmail.com>
2020-03-30 13:09:19 +0200

    Data updated. Needed better source for Fr or some adjustments

--- granular_cases_europe/france_update.py
+++ granular_cases_europe/france_update.py
@@ -6,14 +6,20 @@ our target official website:
 https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv
     """
     import pandas as pd
+    from time import sleep
     data_fr = pd.read_csv("./granular_cases_europe/France.csv")
-    data_raw = pd.read_csv("https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv")
-    data_imported = pd.DataFrame({"country":"France", "granularity":data_raw["granularite"], "name":data_raw["maille_nom"], "confirmed_infected": data_raw["cas_confirmes"], "dead": data_raw["deces"], "recovered": data_raw["reanimation"], "timestamp": data_raw["date"]})
+    dat_raw = pd.read_csv("https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv")
+    dat_reg = dat_raw.query('granularite == "region"')
+    dat_raw = dat_reg.groupby(['date', 'maille_nom']).agg({'cas_confirmes': 'max', 'deces': 'max', 'reanimation':'max'}).reset_index()
+    data_imported = pd.DataFrame({"country":"France", "region":dat_raw["maille_nom"], "confirmed_infected": dat_raw["cas_confirmes"], "dead": dat_raw["deces"], "recovered": dat_raw["reanimation"], "timestamp": dat_raw["date"]})
     if data_fr['timestamp'].equals(data_imported['timestamp']):
-## if this is true, there are no differences (NO UPDATES)
+    ## if this is true, there are no differences (NO UPDATES)
         print("No updates found")
     else:
-        data_fr = data_imported.combine_first(data_fr)
-        data_fr.to_csv("./granular_cases_europe/France.csv", index = False)
+        data_imported.to_csv("./granular_cases_europe/France.csv", index = False)
         print('Following updates added:\n')
         return(data_fr)
+
+## NOTE: From 25.03.2020 many missing values of confirmed_infected
+##       From 27.03.2020 no data for confirmed_infected
+## TRY to find another source or contact them and ask
