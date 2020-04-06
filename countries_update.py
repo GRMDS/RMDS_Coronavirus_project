d1391376bb63b881d1f8e42900533dd58d6524b9
teoten <teoten@gmail.com>
2020-04-06 17:34:20 +0200

    Data updated. No updates for France

---
+++ countries_update.py
@@ -0,0 +1,9 @@
+from granular_cases_europe import france_update as fr
+from granular_cases_europe import spain_update as sp
+from granular_cases_europe import italy_update as it
+from granular_cases_europe import germany_update as ge
+
+fr.france_update()
+sp.spain_update()
+it.italy_update()
+ge.germany_update()
