ce7a35b8cdd9d9ff7a9239a68207464975b77999
teoten <teoten@gmail.com>
2020-04-09 18:18:58 +0200

    Data updated

--- countries_update.py
+++ countries_update.py
@@ -3,7 +3,11 @@ from granular_cases_europe import spain_update as sp
 from granular_cases_europe import italy_update as it
 from granular_cases_europe import germany_update as ge
 
+print('Searching updates for France \n')
 fr.france_update()
+print('Searching updates for Spain \n')
 sp.spain_update()
+print('Searching updates for Italy \n')
 it.italy_update()
+print('Searching updates for Germany \n')
 ge.germany_update()
