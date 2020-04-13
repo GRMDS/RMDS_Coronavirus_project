a0846d383c81f63261f90480235be6bf2c59fa8f
Jidong0726 <jidongli@DESKTOP-OEOIVK6.localdomain>
2020-04-13 02:49:37 -0700

    add community survey data

---
+++ data_ingress/Community_Survey_data.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thr Apr 9 16:31:22 2020
+
+@author: jidong
+Merge US community survey data into MongoDB
+"""
+
+import pymongo
+from pymongo import MongoClient
+import numpy as np
+import requests
+
+BASE_URL = 'https://api.census.gov'
+PATH = 'data/2018/acs/acsse'
+ATTRIBUTES = ['name', 'K200201_001E', 'K200201_002E', 'K200201_003E', 'K200201_004E', 'K200201_005E', 
+              'K200201_006E', 'K200201_007E','K200103_001E','K201703_001E','K201703_002E','K201703_007E']
+ATTRIBUTES_LABEL = ['name','race_total','White', 'Black or African American', 'American Indian and Alaska Native',
+                    'Asian', 'Native Hawaiian and Other Pacific Islander', 'Some other race', 'Median age',
+                    'poverty_total','below poverty level','above poverty level']
+GRANULARITY = 'county'
+
+
+def get_json_from_api():
+    params = {
+        'get': ','.join([attr.upper() for attr in ATTRIBUTES]),
+        'for': f'{GRANULARITY}:*',
+    }
+    response = requests.get(url=f'{BASE_URL}/{PATH}', params=params)
+    county_data = response.json()
+    return county_data
+
+def insert_data(data, mongodb_host: str,
+    mongodb_port: int,
+    mongodb_dest_database: str,
+    mongodb_dest_collection: str,
+    mongodb_username: str,
+    mongodb_password: str,):
+    
+    mongo_client = MongoClient(
+            mongodb_host,
+            mongodb_port,
+            username=mongodb_username,
+            password=mongodb_password,
+            authSource=mongodb_dest_database,
+            authMechanism='SCRAM-SHA-256',
+        )
+    db = mongo_client[mongodb_dest_database]
+    collection = db[mongodb_dest_collection]
+    '''
+    collection.update_many({},
+                      {"$set": {"white_rate": None, "black_rate": None, 
+                                "AIAN": None, "asian_rate": None, "NHPI": None, 
+                                "other_race": None, "median_age": None, "below_poverty": None,
+                                "above_poverty": None}},
+                      upsert=False, array_filters=None)
+    '''
+    
+    for elem in data:
+        countyname = elem[0].split(', ')[0]
+        statename = elem[0].split(', ')[1]
+        if elem[1]!=None:
+            white_rate = float(elem[2])/float(elem[1])
+            black_rate = float(elem[3])/float(elem[1])
+            AIAN = float(elem[4])/float(elem[1])
+            asian_rate = float(elem[5])/float(elem[1])
+            NHPI = float(elem[6])/float(elem[1])
+            other = float(elem[7])/float(elem[1])
+        else:
+            white_rate, black_rate, AIAN, asian_rate, NHPI, other = [None]*6
+        
+        median_age = float(elem[8])
+        if elem[9]!=None:
+            below_poverty = float(elem[10])/float(elem[9])
+            above_poverty = float(elem[11])/float(elem[9])
+        else:
+            below_poverty, above_poverty = None, None
+        
+        collection.update_one({"$and": [{"state_name": {"$eq": statename}},{"county_name": {"$eq": countyname}}]},
+                              {"$set": {"white_rate": white_rate, "black_rate": black_rate, 
+                               "AIAN": AIAN, "asian_rate": asian_rate, "NHPI": NHPI, 
+                               "other_race": other, "median_age": median_age, "below_poverty": below_poverty,
+                               "above_poverty": above_poverty}},
+                              upsert=False)
+        
+
+    return collection
+    
+
+
+
+if __name__ == '__main__':
+    x = get_json_from_api()
+    y = insert_data(x[1:], '3.101.18.8', 27017, 'COVID19-DB', 'counties', 'Your Username', 'Your Password')
