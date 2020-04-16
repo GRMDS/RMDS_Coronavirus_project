f0d7835f8aea50b26749216be02e7de464b35086
Jidong0726 <jidongli@DESKTOP-OEOIVK6.localdomain>
2020-04-15 19:44:19 -0700

    add country stringency

--- data_ingress/countries.py
+++ data_ingress/countries.py
@@ -19,6 +19,7 @@ from enum import Enum
 from pymongo import MongoClient
 import requests
 from typing import Dict, List
+import pycountry
 
 BASE_URL = 'https://en.wikipedia.org/wiki/'
 PATH = 'List_of_countries_by_population_(United_Nations)'
@@ -130,13 +131,57 @@ def get_country_data(
     mongodb_password,
     )
     
+ 
+'''
+insert country level Stringency Index (quantified policy score) from Oxford Univ
+https://www.bsg.ox.ac.uk/sites/default/files/Calculation%20and%20presentation%20of%20the%20Stringency%20Index.pdf
+Use pycountry to covert alpha_3 country code to country name
+insert to new collection country_stringency
+'''
+def insert_country_stringency(enddate: str, 
+    mongodb_host: str,
+    mongodb_port: int,
+    mongodb_dest_database: str,
+    mongodb_dest_collection: str,
+    mongodb_username: str,
+    mongodb_password: str,):
     
+    response = requests.get('https://covidtrackerapi.bsg.ox.ac.uk/api/stringency/date-range/2020-02-01/'+enddate)
+    data = list(response.json()['data'].values())
+    list_data = []
+    for elem in data:
+        temp = list(elem.values())
+        for elem2 in temp:
+            elem2['date_value'] = datetime.strptime(elem2['date_value'], '%Y-%m-%d')
+            country = pycountry.countries.get(alpha_3 = elem2['country_code'])
+            
+            # HK, Macon, Taiwan belong to China
+            if country!=None and country not in ['Taiwan', 'Macao', 'Hong Kong']:
+                elem2['country'] = country.name
+                list_data.append(elem2)
+    
+    mongo_client = MongoClient(
+            mongodb_host,
+            mongodb_port,
+            username=mongodb_username,
+            password=mongodb_password,
+            authSource=mongodb_dest_database,
+            authMechanism='SCRAM-SHA-256',
+        )
+    db = mongo_client[mongodb_dest_database]
+    collection = db[mongodb_dest_collection]
+    
+    collection.drop()
 
+    collection.insert_many(list_data)
+    
+    return list_data
     
     
     
 
 if __name__ == '__main__':
+    '''
     parser = argparse.ArgumentParser(description='Pulls wikipedia world population data to find population distribution by countries')
     
     parser.add_argument('--mongodb_host', help='The hostname of the mongodb server', default='localhost')
@@ -147,4 +192,5 @@ if __name__ == '__main__':
     parser.add_argument('--mongodb_password', help='The password of the mongodb instance', default=None)
     
     get_country_data(**vars(parser.parse_args()))
-    
+    '''
+    x = insert_country_stringency('2020-04-14', '3.101.18.8', 27017, 'COVID19-DB', 'Country_stringency', 'Your Username', 'Your Password')
